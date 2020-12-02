009a330166ce734f30c46edb742a1d3c3552976a
Giulio Piccini <giulio.piccini94@gmail.com>
2020-12-02 17:53:40 +0100

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    /*
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -13,6 +14,7 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    */
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     /* MANIFEST
