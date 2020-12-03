aea7a8d9506cec08e06690fa6df1a3320950b78a
Giulio Piccini <giulio.piccini94@gmail.com>
2020-12-04 00:30:47 +0100

    setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,7 @@ import ScrollIcon from "../components/scrollIcon/scrollIncon";
 export const StyledView = styled.div`
   height: 100vh;
   width: 100vw;
+  min-height: -webkit-fill-available;
 
   .headingWrapper {
     position: absolute;
