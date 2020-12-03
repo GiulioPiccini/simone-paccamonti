fdcb7190be774c536e440e8dfe14afae6a1e051a
Giulio Piccini <giulio.piccini94@gmail.com>
2020-12-03 14:43:39 +0100

    setup

--- src/components/scrollIcon/scrollIncon.js
+++ src/components/scrollIcon/scrollIncon.js
@@ -6,7 +6,7 @@ export const StyledScrollIcon = styled.div`
     left: 50%;
     width: 40px;
     height: 40px;
-    bottom: 3rem;
+    bottom: 60px;
     margin: -20px 0 0 -20px;
     -webkit-transform: rotate(45deg);
     border-left: none;
@@ -14,25 +14,25 @@ export const StyledScrollIcon = styled.div`
     border-right: 2px ${({ theme }) => theme.primaryColor} solid;
     border-bottom: 2px ${({ theme }) => theme.primaryColor} solid;
     cursor: pointer;
+
     :before {
         position: absolute;
-        left: 50%;
         content: '';
         width: 20px;
         height: 20px;
         top: 50%;
-        margin: -10px 0 0 -10px;
+        margin: -10px 0 0 10px;
         border-left: none;
         border-top: none;
         border-right: 1px ${({ theme }) => theme.primaryColor} solid;
         border-bottom: 1px ${({ theme }) => theme.primaryColor} solid;
-        animation-duration: 2s;
+        animation-duration: 1.7s;
         animation-iteration-count: infinite;
         animation-name: arrow;
     }
-      @keyframes arrow {
+    @keyframes arrow {
         0% { opacity: 1; }
-        100% { opacity: 0; transform: translate(-10px, -10px); }
+        100% { opacity: 0; transform: translate( -10px, -10px)   }
        }
 
 `;
